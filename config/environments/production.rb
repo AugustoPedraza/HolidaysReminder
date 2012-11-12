6a97442201ac6c1e571349a4753d37dc301b3579
Augusto <augusto_pedraza@hotmail.com>
2012-11-12 14:15:34 -0300

    Actualizar configuracion de producción de servidor de email.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,8 +48,15 @@ HolidaysReminder::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
-  # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method       = :smtp
+  config.action_mailer.smtp_settings         = {
+      address:              "smtp.gmail.com",
+      port:                 587,
+      user_name:            'myholidaysreminder@gmail.com',
+      password:             '1234pass',
+      authentication:       'plain',
+      enable_starttls_auto: true  }
 
   # Enable threaded mode
   # config.threadsafe!
